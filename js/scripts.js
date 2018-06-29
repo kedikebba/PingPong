4fa1abac9be5953e9bcc88086baad67be97110ad
Kedi Kebba <kebbakedi@gmail.com>
2018-06-29 14:27:26 +0300

    Defines the Business Logic and User Interface Logic

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+//Business Logic
+function pingPong(){
+  for ( var generatedNumbers = 1; generatedNumbers <= userInput; generatedNumbers++;){
+    if (generatedNumbers%3===0){
+      //Push to ul list by printing Ping
+    }else if (generatedNumbers%5===0) {
+      //Push to ul list by printing Ping
+    }else if (generatedNumbers%15===0){
+      //Push to ul list by printing PingPong
+    }else {
+      //Push to UL List the number in ordered List
+    }
+  }
+}
+
+
+
+
+
+//User Logic
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
